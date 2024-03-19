50da4ccdaaaf978e9a8afb555518b29b27d9e20b
DVP <dvp@TechAcademy01s-MacBook-Pro.local>
2024-03-19 09:55:12 +0300

    Initial Commit

---
+++ Tech Academy News App Hamad/Tech_Academy_News_App_HamadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_Academy_News_App_HamadApp.swift
+//  Tech Academy News App Hamad
+//
+//  Created by DVP on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tech_Academy_News_App_HamadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tech Academy News App HamadUITests/Tech_Academy_News_App_HamadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tech_Academy_News_App_HamadUITestsLaunchTests.swift
+//  Tech Academy News App HamadUITests
+//
+//  Created by DVP on 19/03/2024.
+//
+
+import XCTest
+
+final class Tech_Academy_News_App_HamadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
